5255e114bc0b244322b553d91b4e8be5b6b2fe15
Dan Cruzat <thecruzat@gmail.com>
2021-09-02 22:13:17 -0700

    update meta

--- pages/index.js
+++ pages/index.js
@@ -30,27 +30,27 @@ export default function Home() {
   return (
     <div>
         <Head>
-            <title>{ metaFings.title }</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <meta name="description" content={ metaFings.description } />
-            <meta name="keywords" content="Cruzat, Portland, producer, web developer, front end" />
-            <meta name="author" content={ metaName } />
-            <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:site" content={ metaFings.twitter } />
-            <meta name="twitter:creator" content={ metaFings.twitter } />
-            <meta name="twitter:title" content={ metaFings.title } />
-            <meta name="twitter:description" content={ metaFings.description } />
-            <meta name="twitter:image" content={ metaFings.image } />
-            <meta name="twitter:image:alt" content={ metaName } />
-            <meta property="og:url" content={ metaFings.url } />
-            <meta property="og:title" content={ metaFings.title } />
-            <meta property="og:site_name" content={ metaFings.title } />
-            <meta property="og:description" content={ metaFings.description } />
-            <meta property="og:image" content={ metaFings.image } />
+          <title>{ metaFings.title }</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="description" content={ metaFings.description } />
+          <meta name="keywords" content="Cruzat, Portland, producer, web developer, front end" />
+          <meta name="author" content={ metaName } />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content={ metaFings.twitter } />
+          <meta name="twitter:creator" content={ metaFings.twitter } />
+          <meta name="twitter:title" content={ metaFings.title } />
+          <meta name="twitter:description" content={ metaFings.description } />
+          <meta name="twitter:image" content={ metaFings.image } />
+          <meta name="twitter:image:alt" content={ metaName } />
+          <meta property="og:url" content={ metaFings.url } />
+          <meta property="og:title" content={ metaFings.title } />
+          <meta property="og:site_name" content={ metaFings.title } />
+          <meta property="og:description" content={ metaFings.description } />
+          <meta property="og:image" content={ metaFings.image } />
 
-            <link rel="icon" type="image/x-icon" href="./assets/samurai.ico" />
-            <link href='//fonts.googleapis.com/css?family=Rock+Salt|Roboto+Slab' rel='stylesheet' type='text/css' />
-            <link href='//stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' rel='stylesheet' type='text/css' />
+          <link rel="icon" type="image/x-icon" href="./assets/samurai.ico" />
+          <link href='//fonts.googleapis.com/css?family=Rock+Salt|Roboto+Slab' rel='stylesheet' type='text/css' />
+          <link href='//stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' rel='stylesheet' type='text/css' />
         </Head>
         <TopRail />
         <Background />
